d79ce38b0d5c26c891c3c28f9db84baf4cd88d5d
@shijiang.guo <shijiang.guo@bluehelix.com>
2021-09-13 00:14:55 +0800

    fix

--- common/templatetags/cf_tags.py
+++ common/templatetags/cf_tags.py
@@ -39,56 +39,12 @@ def ktd_places(value):
         else dec_value.normalize()
     )
 
-
-@register.filter(name="user_order_status")
-def user_order_status(value):
+@register.filter(name="event_status")
+def event_status(value):
     return {
-        'NO_PAY': '未支付',
-        'PAY_SUCCESS': '等待发货',
-        'SEND_GOODS': '等待收货',
-        'RECV_GOODS': '等待评价',
-        'RETURN_GOODS': '发起退货换货',
-        'CANCLE_RETURN_GOODS': '取消退货换货',
-        'RETURN_SELLER_RJT': '卖家拒绝',
-        'RETURN_SELLER_ACPT': '卖家同意',
-        'BUYER_APPOVAL': '买家申诉',
-        'APPOVAL_FAIL': '买家申诉失败',
-        'APPOVAL_SUCCESS': '订单申诉成功',
-        'BUYER_SEND_GOODS': '买家已发货, 卖家等待收货',
-        'BUYER_RECV_GOODS': '买家已发货',
-        'SELLER_RECV_GOODS': '卖家已经收到货',
-        'SELLER_SEND_GOODS': '卖家已发货，买家等待收货',
-        'SELLER_RETURN_MNY': '卖家已退款',
-        "FINISH": "已完成"
+        'WAIT_CHECK': '审核中',
+        'WAIT_SOLUTE': '解决中',
+        'WAIT_BACK': '待解决',
+        'FINISH': '已完成'
     }.get(value, '')
-
-
-@register.filter(name="marchant_order_status")
-def marchant_order_status(value):
-    return {
-        'NO_PAY': '待付款',
-        'PAY_SUCCESS': '等待发货',
-        'SEND_GOODS': '您已发货，等待客户收货',
-        'RECV_GOODS': '客户已收货',
-        'RETURN_GOODS': '退货换货',
-        'RETURN_SELLER_RJT': '您已拒绝退货, 用户可能会发起申述',
-        'RETURN_SELLER_ACPT': '您已同意退货，如果已发货，等待卖家将货物邮寄给您，再退款',
-        'BUYER_APPOVAL': '用户发起申诉, 等待平台处理',
-        'APPOVAL_SUCCESS': '订单申诉成功',
-        'SELLER_RETURN_MNY': '您已退款，该订单已经完成',
-        "FINISH": "已完成"
-    }.get(value, '')
-
-
-@register.filter(name="wallet_record_status")
-def wallet_record_status(value):
-    return {
-        'Checking': '审核中(未锁定)',
-        'Trading': '交易中',
-        'SendOut': '已发出',
-        'Success': '成功',
-        'Fail': '失败',
-        'CheckPass': '审核通过',
-        'CheckRefuse': '审核拒绝',
-
-    }.get(value, '')
+   #
