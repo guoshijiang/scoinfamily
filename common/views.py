5a6f9e622dfb150ecbdb4f31e819a51993bdbb3a
@shijiang.guo <shijiang.guo@bluehelix.com>
2021-09-14 19:11:38 +0800

    fix

--- common/views.py
+++ common/views.py
@@ -13,10 +13,10 @@ def index(request):
     nav_bar = "index"
     user_agt = judge_pc_or_mobile(request.META.get("HTTP_USER_AGENT"))
     lst_event = Event.objects.filter(
-        status__in=["WAIT_SOLUTE", "WAIT_BACK", "FINISH"]
-    ).order_by("-id")[0:10]
-    lst_arctcle = Article.objects.filter(is_active=True)[0:3]
-    lst_tools = Tools.objects.filter(is_active=True)[0:8]
+        status__in=["WAIT_CHECK", "WAIT_SOLUTE", "WAIT_BACK", "FINISH"]
+    ).order_by("-id")[:10]
+    lst_arctcle = Article.objects.filter(is_active=True)[:3]
+    lst_tools = Tools.objects.filter(is_active=True)[:8]
     if user_agt is False:
         return render(request, 'web/pages/index.html', locals())
     else:
